c5d27559af58254cbd892f71d4ed3aceec64a438
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-06-22 20:10:17 +0530

    some more practice on operators

--- src/exercise5.java
+++ src/exercise5.java
@@ -11,10 +11,5 @@ public class exercise5 {
         int eresu = num1 + 8;
         System.out.println("Here is you encrypted number "+eresu);
 
-        System.out.println("Enter your number to decrypt: ");
-        int num2 = num.nextInt();
-
-        int dersu = num2 - 8;
-        System.out.println("Here is your decrypted number: "+dersu);
     }
 }
