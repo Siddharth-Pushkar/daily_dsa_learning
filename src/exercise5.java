315198c3316030ec9aa9e168d271b052e2985ff5
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-06-22 23:45:21 +0530

    higher operator exercise

--- src/exercise5.java
+++ src/exercise5.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
-
+import java.lang.Math;
 
 public class exercise5 {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
 
-        System.out.println("Enter your number to encrypt: ");
-        int num1 = num.nextInt();
+        System.out.println("Enter your v here: ");
+        int v = num.nextInt();
 
-        int eresu = num1 + 8;
-        System.out.println("Here is you encrypted number "+eresu);
+        System.out.println("Enter your u here: ");
+        int u = num.nextInt();
+
+        System.out.println("Enter your a here: ");
+        int a = num.nextInt();
+
+        double final_exp ;
+
+        final_exp = ((Math.pow(v,2) - (Math.pow(u,2)) / (2*a*5)));
+
+        System.out.println("Your final result is :" + final_exp);
 
     }
 }
