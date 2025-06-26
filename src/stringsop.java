d23e929c45775cbde27b7fa55ea33d2b91c7c29c
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-06-26 17:39:45 +0530

    string operation

---
+++ src/stringsop.java
@@ -0,0 +1,16 @@
+public class stringsop {
+
+    public static void main(String[] args) {
+
+
+        String name ;
+        String edited;
+        name = "this is a text line for testing";
+
+        edited = name.toUpperCase();
+        System.out.println(edited);
+
+
+
+    }
+}
