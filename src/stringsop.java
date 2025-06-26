85d73c0babe64b202e66c08071d78420a4edbf00
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-06-26 17:54:30 +0530

    String operations 1

--- src/stringsop.java
+++ src/stringsop.java
@@ -5,10 +5,17 @@ public class stringsop {
 
         String name ;
         String edited;
+        int editod;
+
         name = "this is a text line for testing";
 
         edited = name.toUpperCase();
+        edited = name.toLowerCase();
+        edited = name.trim();
+        edited = name.replace("e","3");
+        editod = name.indexOf("e",12);
         System.out.println(edited);
+        System.out.println(editod);
 
 
 
