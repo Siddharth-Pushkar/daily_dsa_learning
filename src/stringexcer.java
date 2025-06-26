e33b4e486013b26468588b4f745461c337322a32
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-06-26 21:02:03 +0530

    String excercise

---
+++ src/stringexcer.java
@@ -0,0 +1,16 @@
+public class stringexcer {
+
+    public static void main(String[] args) {
+        String name;
+        String edited;
+
+
+        name = "This is a text line for testing purpose";
+        System.out.println(name.replace(" ","_"));
+
+        edited = name.replace(" ","_");
+        System.out.println(edited);
+
+
+    }
+}
