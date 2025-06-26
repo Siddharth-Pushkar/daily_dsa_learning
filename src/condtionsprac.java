a6a57720ec214fe431852fdf30d8e412ef6e6a15
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-06-26 22:42:48 +0530

    Condition operations

--- src/condtionsprac.java
+++ src/condtionsprac.java
@@ -1,5 +1,17 @@
 public class condtionsprac {
 
-    
+    public static void main(String[] args) {
+        int marks = 82;
+
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 75) {
+            System.out.println("Grade B");
+        } else {
+            System.out.println("Grade C");
+        }
+
+
+    }
 
 }
