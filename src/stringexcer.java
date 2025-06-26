c6a60a4557a2e25a32a094d2e06d499451a19ebd
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-06-26 21:10:27 +0530

    String excercise 2

--- src/stringexcer.java
+++ src/stringexcer.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class stringexcer {
 
     public static void main(String[] args) {
         String name;
         String edited;
+
+        Scanner chang = new Scanner(System.in);
 
 
         name = "This is a text line for testing purpose";
@@ -10,6 +14,15 @@ public class stringexcer {
 
         edited = name.replace(" ","_");
         System.out.println(edited);
+
+        System.out.println("Enter your name here: ");
+        String name2 = chang.next();
+
+        System.out.println("Enter your age here: ");
+        int agi = chang.nextInt();
+
+        System.out.printf("Hi, My name is %s , and i am %d years old.",name2, agi);
+
 
 
     }
