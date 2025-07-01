772965ca3fc20cef40baa7dbdd4ad35f00118b2d
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-07-01 20:55:26 +0530

    condtion practice 3

--- src/condtionsprac.java
+++ src/condtionsprac.java
@@ -12,51 +12,66 @@ public class condtionsprac {
 //        } else {
 //            System.out.println("Grade C");
 //        }
-        Scanner inputop = new Scanner(System.in);
+//        Scanner inputop = new Scanner(System.in);
+//
+//        System.out.println("Enter your name here: ");
+//        String namech = inputop.next();
+//
+//        System.out.println("Enter your age here: ");
+//        int ageo = inputop.nextInt();
+//
+//        System.out.println("Enter your location here: ");
+//        String loco = inputop.next();
+//
+//        if (namech.charAt(0) == 'S'){
+//            System.out.println("Your name start with S");
+//        }
+//        else {
+//            System.out.println("Your name does not start with S");
+//        }
+//
+//        if (ageo > 18){
+//            System.out.println("You are an adult");
+//        } else if (ageo >= 100) {
+//            System.out.println("Invalid age");
+//        }
+//        else {
+//            System.out.println("You are underage");
+//        }
+//
+//        switch (loco) {
+//
+//            case "Delhi":
+//                System.out.println("You live close to me");
+//                break;
+//
+//            case "Goa":
+//                System.out.println("You live very far from me");
+//                break;
+//
+//            case "Bangalore":
+//                System.out.println("You live very far from me");
+//                break;
+//
+//            default:
+//                System.out.println("You live no where close to me");
+//                break;
+//        }
 
-        System.out.println("Enter your name here: ");
-        String namech = inputop.next();
 
-        System.out.println("Enter your age here: ");
-        int ageo = inputop.nextInt();
+        Scanner urlop = new Scanner(System.in);
 
-        System.out.println("Enter your location here: ");
-        String loco = inputop.next();
+        System.out.println("Enter your URL here: ");
+        String urlcheck = urlop.next();
 
-        if (namech.charAt(0) == 'S'){
-            System.out.println("Your name start with S");
+        if (urlcheck.endsWith(".com")){
+            System.out.println("It is a Commercial Website");
+        } else if (urlcheck.endsWith(".org")) {
+            System.out.println("It is a Organization Website");
+        } else if (urlcheck.endsWith(".in")) {
+            System.out.println("It is an Indian Website");
         }
-        else {
-            System.out.println("Your name does not start with S");
-        }
 
-        if (ageo > 18){
-            System.out.println("You are an adult");
-        } else if (ageo >= 100) {
-            System.out.println("Invalid age");
-        }
-        else {
-            System.out.println("You are underage");
-        }
-
-        switch (loco) {
-
-            case "Delhi":
-                System.out.println("You live close to me");
-                break;
-
-            case "Goa":
-                System.out.println("You live very far from me");
-                break;
-
-            case "Bangalore":
-                System.out.println("You live very far from me");
-                break;
-
-            default:
-                System.out.println("You live no where close to me");
-                break;
-        }
 
 
     }
