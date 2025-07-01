8e6186e3aa043cc2412dfcba72f84234464354b8
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-07-01 20:19:15 +0530

    condition practice

--- src/condtionsprac.java
+++ src/condtionsprac.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class condtionsprac {
 
     public static void main(String[] args) {
@@ -9,6 +11,33 @@ public class condtionsprac {
             System.out.println("Grade B");
         } else {
             System.out.println("Grade C");
+        }
+        Scanner inputop = new Scanner(System.in);
+
+        System.out.println("Enter your name here: ");
+        String namech = inputop.next();
+
+        System.out.println("Enter your age here: ");
+        int ageo = inputop.nextInt();
+
+        System.out.println("Enter your location here: ");
+        String loco = inputop.next();
+
+        if (namech.charAt(0) == 'S'){
+            System.out.println("Your name start with S");
+        }
+        else {
+            System.out.println("Your name does not start with S");
+        }
+
+
+        if (ageo < 18){
+            System.out.println("You are not an adult");
+        } else if (ageo >= 100) {
+            System.out.println("Invalid age");
+        }
+        else {
+            System.out.println("You are underage");
         }
 
 
