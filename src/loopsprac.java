789626d2a0d2174f76facb7968d4c66cd264a62e
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-07-03 17:54:00 +0530

    Loops 1

---
+++ src/loopsprac.java
@@ -0,0 +1,13 @@
+public class loopsprac {
+
+    public static void main(String[] args) {
+
+        char staro ;
+        staro = '*';
+        int run;
+
+        for (run=0; run!=5; run++){
+            System.out.print(staro);
+        }
+    }
+}
