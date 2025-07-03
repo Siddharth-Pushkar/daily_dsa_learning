991c524ef820d62910154d32f65e58814efc2866
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-07-03 21:54:37 +0530

    loops

--- src/loopsprac.java
+++ src/loopsprac.java
@@ -2,15 +2,15 @@ public class loopsprac {
 
     public static void main(String[] args) {
 
-        int numup;
+        int numup = 0;
 
-        for (int i = 0; i < 10; ){
-
-            for (numup = 0; numup != 10; numup++) {
+        for (int i = 0; i != 10; i++){
+            for (numup = 0; numup<=i;){
                 System.out.print(numup);
+                numup++;
             }
             System.out.println();
-            i++;
         }
+
     }
 }
