0377ff04ee07b28a34f842f5258906f6c95c4868
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-07-05 17:29:50 +0530

    Loops 8

--- src/loopsprac.java
+++ src/loopsprac.java
@@ -12,10 +12,30 @@ public class loopsprac {
             System.out.println();
         }
 
+
+        for (int i4 = 0; i4 != 5; i4++){
+            for (int j2 = 1; j2 <= i4; j2++) {
+                System.out.print(j2);
+            }
+            System.out.println();
+        }
+
+
+
+
         for (int i1 = 0; i1 <=20; i1++){
-            if (i1 % 2 == 0){
-                System.out.print(i1+" ");
+            if (i1 % 2 == 0) {
+                System.out.print(i1 + " ");
             }
         }
+        System.out.println();
+
+        int nn = 5;
+        int sum = 0;
+
+        for (int i2 = 0; i2<=nn; i2++){
+            sum = sum + i2;
+        }
+        System.out.println("the sum is " +sum);
     }
 }
