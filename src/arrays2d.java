15669768fba1e1e527baef70398838a28f00f0e0
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-08-06 20:15:10 +0530

    2d array 1

---
+++ src/arrays2d.java
@@ -0,0 +1,23 @@
+public class arrays2d {
+    public static void main(String[] args) {
+
+        int [][] dirmatrix = {
+                {2,4,6},
+                {1,3,5},
+                {5,10,15}
+        };
+
+        int [][] indmatrix = new int[3][3];
+
+        indmatrix [0][0] = 3;
+        indmatrix [0][1] = 3;
+        indmatrix [0][2] = 3;
+        indmatrix [1][0] = 3;
+        indmatrix [1][1] = 3;
+        indmatrix [1][2] = 3;
+        indmatrix [2][0] = 3;
+        indmatrix [2][1] = 3;
+        indmatrix [2][2] = 3;
+
+    }
+}
