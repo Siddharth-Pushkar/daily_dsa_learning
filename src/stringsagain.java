71027bd7c8aaf6fa53814bdc072ccd6496f47e1c
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-09-19 21:28:46 +0530

    strings more

---
+++ src/stringsagain.java
@@ -0,0 +1,22 @@
+public class stringsagain {
+
+    public static void main(String[] args) {
+
+        String s = "some";
+        System.out.println(s);
+        s = s.concat("thing");
+        System.out.println(s);
+
+        System.out.println(s.replace("me","n"));
+        s = s.replace("me","n");
+        System.out.println(s);
+
+        String ss = "some";
+        char ch = s.charAt(3);
+        String ss1 = "solar";
+        String ss2 = "some";
+
+        System.out.println();
+
+    }
+}
