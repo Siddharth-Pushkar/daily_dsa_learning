35bdca25551ae13f29b93a123c1b69682afbd466
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-10-04 11:52:08 +0530

    practice

--- src/practicemethods.java
+++ src/practicemethods.java
@@ -1,2 +1,13 @@
 public class practicemethods {
+    public static void takinginto(int num){
+        if (num < 18){
+            System.out.println("Not an adult");
+            return;
+        }
+        System.out.println("An adult");
+    }
+
+    public static void main(String[] args) {
+        takinginto(16);
+    }
 }
