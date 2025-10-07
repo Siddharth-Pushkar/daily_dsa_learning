a3d5249082066f8c4d638a83f21eb5f571f9cbfd
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-10-07 11:21:26 +0530

    methods

--- src/practicemethods.java
+++ src/practicemethods.java
@@ -8,11 +8,19 @@ public class practicemethods {
     }
 
 
+    public static void recurnum(int num2) {
+        if (num2 == 0) return;
+        System.out.print(num2+" ");
+        recurnum(num2-1);
+    }
 
+    public static void sumnum(int num3) {
+        if (num3 == 0) return;
 
-
+    }
 
     public static void main(String[] args) {
         takinginto(16);
+        recurnum(15);
     }
 }
