61c5be1967d2be8c393439396294145584e3a887
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2025-12-03 15:27:32 +0530

    filework 2

--- src/filework.java
+++ src/filework.java
@@ -1,2 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+
 public class filework {
+
+    public static void main(String[] args) {
+
+        File filna = new File("data.txt");
+        if (filna.exists()){
+            System.out.println(filna.getName());
+            System.out.println(filna.getPath());
+            System.out.println(filna.length());
+        }
+
+        try{
+            if (filna.createNewFile()){
+                System.out.println("File is created sucessfully");
+            } else {
+                System.out.println("File already exists");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();}
+
+    }
 }
