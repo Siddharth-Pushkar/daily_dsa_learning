99de0cba93c91c39e1515115578e8706508f25f6
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2026-01-08 17:11:01 +0530

    vararges 2

--- src/varargandprac.java
+++ src/varargandprac.java
@@ -6,9 +6,7 @@ public class varargandprac {
 
     public static void main(String[] args) {
 
-        int a = inpo(354,4675,4367,547,253,78,34,4);
-
-        System.out.println(a);
+        inpo(354,4675,4367,547,253,78,34,4);
 
     }
 
