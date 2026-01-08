46f59b5c3891ac2b72416d27295d1ea1a2fb7c43
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2026-01-08 17:58:11 +0530

    exercise op

--- src/varargandprac.java
+++ src/varargandprac.java
@@ -6,13 +6,35 @@ public class varargandprac {
 
     public static void main(String[] args) {
 
-        inpo(354,4675,4367,547,253,78,34,4);
+//        inpo(354,4675,4367,547,253,78,34,4);
+//
+//    }
+//
+//    static void inpo(int ...v){
+//        System.out.println(Arrays.toString(v));
+//        // this is the multiple variable thing
+
+
+        Scanner s = new Scanner(System.in);
+        int innum = s.nextInt();
+        boolean checker = isprime(innum);
+        System.out.println(checker);
 
     }
 
-    static void inpo(int ...v){
-        System.out.println(Arrays.toString(v));
-        // this is the multiple variable thing
+    static boolean isprime(int innum) {
 
+        int c = 0;
+
+        if(innum < 0){
+            return false;
+        }
+        while (c < innum){
+            if (c % innum != 0){
+                return true;
+            }
+
+        }
+        return c == innum;
     }
 }
