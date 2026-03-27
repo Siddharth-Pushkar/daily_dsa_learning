f66c8d248b307dbf7ce0da88832c0e7c5bf5c35f
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2026-03-27 21:48:27 +0530

    array work

---
+++ src/arraypractice.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class arraypractice {
+
+
+
+
+    public static void main(String[] args) {
+
+
+        Scanner intop = new Scanner(System.in);
+
+
+
+        int [] arr1 = new int[5];
+
+        for (int i = 0; i< arr1.length; ++i ){
+            int num = intop.nextInt();
+            arr1[i] = num;
+        }
+
+
+        System.out.println(Arrays.toString(arr1)); // This is to print the arrays
+
+
+
+
+    }
+}
