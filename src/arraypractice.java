c0e4286878ae64c44304c2c832bee0df2c7ec6bc
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2026-03-29 18:35:47 +0530

    array work

--- src/arraypractice.java
+++ src/arraypractice.java
@@ -6,34 +6,29 @@ public class arraypractice {
 
     static  int [] arraybuilder(int arrnum){
         Scanner intop = new Scanner(System.in);  // Scanner
-
-
-
         int [] arr1 = new int[arrnum];  // Array build
-
         for (int i = 0; i< arr1.length; ++i ){
             System.out.print("Enter "+i+"th number here: ");
             int num = intop.nextInt();       // Input taker
             arr1[i] = num;
         }
-
-
         return arr1;
     }
 
 
     public static void main(String[] args) {
-
-
         Scanner intop = new Scanner(System.in);  // Scanner
 
+
+
         System.out.print("Enter your Length of Array here: ");
-
         int arrnum = intop.nextInt();      // Input taker
-
         int [] anoi = arraybuilder(arrnum);
+        System.out.println(Arrays.toString(anoi));// This is to print the arrays
 
-        System.out.println(Arrays.toString(anoi)); // This is to print the arrays
+        // Now to make a method of function to add +1 to each of the element in the array.
+
+
 
 
 
