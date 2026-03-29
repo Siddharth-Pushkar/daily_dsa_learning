4bb9bcb11d21b10d251b9e201c9df00f3b3c6204
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2026-03-29 19:26:38 +0530

    array work

--- src/arraypractice.java
+++ src/arraypractice.java
@@ -7,7 +7,6 @@ public class arraypractice {
 
 
     static  int [] arraybuilder(int arrnum, Scanner intop){
-
         int [] arr1 = new int[arrnum];  // Array build
         for (int i = 0; i< arr1.length; ++i ){
             System.out.print("Enter "+i+"th number here: ");
@@ -17,15 +16,13 @@ public class arraypractice {
         return arr1;
     }
 
-    static int [] arradder(int [] anoi, Scanner intop){
-        System.out.print("Enter your amount you want to increase: ");
-        int pluser = intop.nextInt();
+    static int [] arradder(int [] anoi, int pluser){
+        // Now to make a method of function to add +1 to each of the element in the array.
         for (int j = 0; j<anoi.length; j++){
             anoi[j] = anoi[j] + pluser;
         }
         return anoi;
     }
-
 
     public static void main(String[] args) {
         Scanner intop = new Scanner(System.in);     // Scanner
@@ -35,15 +32,10 @@ public class arraypractice {
         int [] anoi = arraybuilder(arrnum , intop);
         System.out.println(Arrays.toString(anoi));// This is to print the arrays
 
-        // Now to make a method of function to add +1 to each of the element in the array.
-        anoi =  arradder(anoi, intop);
-
+        System.out.print("Enter your amount you want to increase: ");
+        int pluser = intop.nextInt();
+        anoi =  arradder(anoi, pluser);
         System.out.println(Arrays.toString(anoi));
-
-
-
-
-
 
     }
 }
