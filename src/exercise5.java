310400d5a36565cdbbf58382c5bd86b17cd65b26
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2026-06-20 19:42:53 +0530

    get some work done

--- src/exercise5.java
+++ src/exercise5.java
@@ -3,22 +3,7 @@ import java.lang.Math;
 
 public class exercise5 {
     public static void main(String[] args) {
-        Scanner num = new Scanner(System.in);
 
-        System.out.println("Enter your v here: ");
-        int v = num.nextInt();
-
-        System.out.println("Enter your u here: ");
-        int u = num.nextInt();
-
-        System.out.println("Enter your a here: ");
-        int a = num.nextInt();
-
-        double final_exp ;
-
-        final_exp = ((Math.pow(v,2) - (Math.pow(u,2)) / (2*a*5)));
-
-        System.out.println("Your final result is :" + final_exp);
 
 
     }
