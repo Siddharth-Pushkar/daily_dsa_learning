84b666be1e6bcdc5752664a903f42c0ba78efc21
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2026-06-22 17:41:53 +0530

    hashmaps

--- src/hashmaps.java
+++ src/hashmaps.java
@@ -13,11 +13,31 @@ public class hashmaps {
         markos.put(" ",8);
         markos.put("  ",0);
 
-        System.out.println(markos.get(44));
+        System.out.println(markos.get("P"));
         System.out.println(markos.get("C"));
         System.out.println(markos.get("E"));
         System.out.println(markos.get(" "));
         System.out.println(markos.get("  "));
+
+
+
+//      Counting the categories
+        int[] arr= {1,2,1,1,3,1};
+        HashMap<Integer,Integer> set2 = new HashMap<>();
+        int count = 1;
+        for (int i = 0; i<arr.length; ++i){
+            boolean checko = set2.containsKey(arr[i]);
+            if (checko == true){
+                int getval = set2.get(arr[i]);
+                set2.put(arr[i],++getval);
+            } else {
+                set2.put(arr[i],count);
+            }
+        }
+        System.out.println(set2);
+
+
+
 
 
 
