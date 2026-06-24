091d619b1b408265ca5c4e0ab0d89432aa00af7b
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2026-06-24 15:57:04 +0530

    some work on hashmaps and sum

--- src/runningsum.java
+++ src/runningsum.java
@@ -13,5 +13,49 @@ public class runningsum {
         for (int x : enda){
             System.out.print(x + " ");
         }
+        System.out.println();
+
+
+        // pivot index
+
+        int[] arr1 = {1,7,3,6,5,6};
+        int[] rightsum = new int[arr1.length];
+        int[] leftsum = new int[arr1.length];
+
+        int pivot = 0;
+        int sum = 0;
+        int sumr = 0;
+        int currentSum = 0;
+        int resultIndex = 0;
+
+        for (int i = 0; i<arr1.length; ++i){
+            sum = sum + arr1[i];
+            leftsum[i] = sum;
+        }
+//        for (int i = arr1.length-1; i>=0; --i){
+//            sumr = sumr + arr1[i];
+//            rightsum[i] = sumr;
+//        }
+        for (int i = arr1.length - 1; i >= 0; i--) {
+            currentSum += arr1[i];
+            rightsum[resultIndex] = currentSum;
+            resultIndex++;
+        }
+
+        for (int x : rightsum){
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        for (int x : leftsum){
+            System.out.print(x + " ");
+        }
+
+
+
+//        for (int i = 0; i<arr1.length; ++i){
+//
+//            if ()
+//        }
+
     }
 }
