1cd682910c7d68941c27d86d9f78e0bd67d26f08
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2026-06-26 17:44:33 +0530

    two pointers

--- src/twopointers.java
+++ src/twopointers.java
@@ -4,27 +4,19 @@ public class twopointers {
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
-        int[] arr2 = arr;
+
         int left = 0;
         int right = arr.length - 1;
 
         while (left < right){
-
-            arr[left] = arr2[right];
-            arr[right] = arr2[left];
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
 
             left++;
             right--;
         }
 
-        for (int i = 0; i< arr2.length; i++){
-            if (arr[i] != arr2[i]){
-                System.out.println(false);
-                break;
-            }
-        }
-
         System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(arr2));
     }
 }
