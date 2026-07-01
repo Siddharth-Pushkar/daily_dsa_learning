17438734a0055069f456fc9b7142201ea708d7d5
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-07-01 20:44:59 +0530

    some leetcode practice

--- src/addstringalt.java
+++ src/addstringalt.java
@@ -7,30 +7,23 @@ public class addstringalt {
 
         int len1 = str1.length();
         int len2 = str1.length();
-
         int a = 0;
         int b = 0;
         int k = 0;
-
         char[] result = new char[len1+len2];
-
         while (a < len1 && b < len2){
             result[k++] = str1.charAt(a++);
             result[k++] = str2.charAt(b++);
         }
-
         while (a < len1){
             result[k++] = str1.charAt(a++);
         }
-
         while (b < len2){
             result[k++] = str2.charAt(b++);
         }
 
-
         String merged = new String(result);
         System.out.printf(merged);
-
 
     }
 }
