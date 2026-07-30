cd2c3f44a6737a1fdf75ecf4c8302a199aea4ac9
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-07-30 17:57:37 +0530

    reversing vovels in the string

--- src/newworkings.java
+++ src/newworkings.java
@@ -2,7 +2,7 @@ public class newworkings {
 
     public static void main(String[] args) {
 
-        String s = "Metamorphosis";
+        String s = "leetcode";
 
         StringBuilder sb = new StringBuilder(s);
 
@@ -11,13 +11,33 @@ public class newworkings {
         int right = s.length() - 1;
         char lefch = 'z';
         char rightch = 'z';
-        while (left<right){
-
+        while (left<=right){
+            while (left<right){
+                char chl = Character.toLowerCase(s.charAt(left));
+                if (chl == 'a' ||chl == 'e' ||chl == 'i' ||chl == 'o' ||chl == 'u'){
+                    lefch = s.charAt(left);
+                    break;
+                } else {
+                    left++;
+                }
+            }
+            while (right>left){
+                char chr = Character.toLowerCase(s.charAt(right));
+                if (chr == 'a' ||chr == 'e' ||chr == 'i' ||chr == 'o' ||chr == 'u'){
+                    rightch = s.charAt(right);
+                    break;
+                } else {
+                    right--;
+                }
+            }
+            char swap = sb.charAt(left);
+            sb.setCharAt(left,sb.charAt(right));
+            sb.setCharAt(right,swap);
+            left++;
+            right--;
         }
-
-
+        String ss = sb.toString();
         System.out.println(sb);
-
     }
 
 
