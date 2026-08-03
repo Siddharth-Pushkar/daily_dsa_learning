ec4bb368cfce488843bd27b85a683c755a2b1d8a
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-03 19:34:37 +0530

    max vovels substring

--- src/maxvovsubstring.java
+++ src/maxvovsubstring.java
@@ -3,7 +3,39 @@ public class maxvovsubstring {
     public static void main(String[] args) {
 
         String s = "lafhwgnerin";
-        StringBuilder sb = new StringBuilder(s);
+        int k = 3;
+        int right = k;
+        int left = 0;
+        int maxvovels = 0;
+        int currentvovels = 0;
+
+        for (int i = 0; i<k;i++){
+            char ch = Character.toLowerCase(s.charAt(i));
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                currentvovels++;
+            }
+        }
+        maxvovels = currentvovels;
+
+        while (right < s.length()){
+            char chr = Character.toLowerCase(s.charAt(right));
+            char chl = Character.toLowerCase(s.charAt(left));
+            if (chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u'){
+                currentvovels++;
+            } else {
+                currentvovels--;
+            }
+            if (chl == 'a' || chl == 'e' || chl == 'i' || chl == 'o' || chl == 'u'){
+                currentvovels++;
+            } else {
+                currentvovels--;
+            }
+            maxvovels = Math.max(currentvovels,maxvovels);
+            right++;
+            left++;
+        }
+
+        System.out.println(maxvovels);
 
 
     }
