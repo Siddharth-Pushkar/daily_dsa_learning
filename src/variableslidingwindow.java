2e0344aef22510455568c6d582ca938f64c739c2
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-06 20:41:16 +0530

    leetcode 3

--- src/variableslidingwindow.java
+++ src/variableslidingwindow.java
@@ -1,5 +1,23 @@
+import java.util.HashSet;
+
 public class variableslidingwindow {
     public static void main(String[] args) {
+
+        String s = "abcabcabc";
+
+        int right = 0;
+        int left = 0;
+        int maxnrwindow = 0;
+        HashSet<Character> sb = new HashSet<>();
+
+        while (right<s.length()){
+            if (!sb.add(s.charAt(right))){
+                sb.remove(s.charAt(left));
+                left++;
+            } else {
+
+            }
+        }
 
     }
 }
