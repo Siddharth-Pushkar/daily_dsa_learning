f7cdd5f3fb11adb85af62d316bed0b71d0c70663
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-08 20:42:25 +0530

    isomorphic

--- src/maxconsecutiveones.java
+++ src/maxconsecutiveones.java
@@ -1,28 +1,49 @@
+import java.util.HashMap;
+
 public class maxconsecutiveones {
     public static void main(String[] args) {
         System.out.println("This is not a max consecutive ones solution because the GPT has lost its mind and giving me a binary search question to solve.");
 
 
-        System.out.println("Name this a 205 Isomorphic, because that is the question i am doing which used HashMaps");
+        System.out.println("Name this as 205 Isomorphic, because that is the question i am doing which used HashMaps");
 
         String s = "egg";
         String t = "add";
         boolean isittrue = true;
-        int[] indexS = new int[256];
-        int[] indexT = new int[256];
+        HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
 
-            if (indexS[c1] != indexT[c2]) {
+            if (mapST.containsKey(c1) && mapST.get(c1) != c2) {
                 isittrue = false;
-                break;
+            }
+            if (mapTS.containsKey(c2) && mapTS.get(c2) != c1) {
+                isittrue = false;
             }
 
-            indexS[c1] = i + 1;
-            indexT[c2] = i + 1;
+            mapST.put(c1, c2);
+            mapTS.put(c2, c1);
         }
+
+//        int[] indexS = new int[256];
+//        int[] indexT = new int[256];
+        
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            char c1 = s.charAt(i);
+//            char c2 = t.charAt(i);
+//
+//            if (indexS[c1] != indexT[c2]) {
+//                isittrue = false;
+//                break;
+//            }
+//
+//            indexS[c1] = i + 1;
+//            indexT[c2] = i + 1;
+//        }
 
         System.out.println(isittrue);
     }
