9bdce2446407965f9fb288873279cf944e436bed
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-08 20:10:31 +0530

    hashmap questions

--- src/maxconsecutiveones.java
+++ src/maxconsecutiveones.java
@@ -1,2 +1,5 @@
 public class maxconsecutiveones {
+    public static void main(String[] args) {
+        System.out.println("This is not a max consecutive ones solution because the GPT has lost its mind and giving me a binary search question to solve.");
+    }
 }
