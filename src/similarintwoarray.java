6237c5341d747569e2f40d0dcdbefda066528097
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-10 21:52:39 +0530

    Intersection of Two Arrays

--- src/similarintwoarray.java
+++ src/similarintwoarray.java
@@ -1,5 +1,25 @@
+import java.util.HashSet;
+
 public class similarintwoarray {
     public static void main(String[] args) {
+
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> intersect = new HashSet<>();
+        HashSet<Integer> n3 = new HashSet<>();
+
+        for (int num : nums1) {
+            set.add(num);
+        }
+
+        for (int num : nums2) {
+            if (set.contains(num)) {
+                intersect.add(num);
+            }
+        }
+        System.out.println(intersect);
 
     }
 }
