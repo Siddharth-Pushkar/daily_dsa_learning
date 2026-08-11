6f519e0a5a38a31f3b526fbc569b73d02f426f92
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-11 21:05:18 +0530

    first unique char

--- src/firstunique.java
+++ src/firstunique.java
@@ -1,6 +1,27 @@
+import java.util.HashMap;
+
 public class firstunique {
 
     public static void main(String[] args) {
+
+        String s = "leetcode";
+        HashMap<Character, Integer> set = new HashMap<>();
+        int inival = 1;
+        int uniq = s.length();
+        for (int i = 0; i<s.length(); i++){
+            if (set.containsKey(s.charAt(i)) == false){
+                set.put(s.charAt(i), inival);
+            } else {
+                set.put(s.charAt(i), set.get(s.charAt(i) + 1));
+            }
+        }
+        for (int i = 0; i<s.length(); i++){
+            int schar = set.get(s.charAt(i));
+            if (schar < uniq){
+                uniq = s.charAt(i); }
+        }
+
+        System.out.println(uniq);
 
     }
 }
