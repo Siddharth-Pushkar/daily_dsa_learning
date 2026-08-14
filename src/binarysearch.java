8f3286054cb52550ad839860ba234207609c5035
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-14 19:14:35 +0530

    binary search

--- src/binarysearch.java
+++ src/binarysearch.java
@@ -1,5 +1,10 @@
 public class binarysearch {
     public static void main(String[] args) {
 
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int k = 6;
+        int left = 0;
+        int right = nums.length - 1;
+
     }
 }
