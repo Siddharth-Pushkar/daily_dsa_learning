415c13712ae104d0841b9567626d124e7a3ab7c6
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-15 11:10:09 +0530

    binary search subtop

--- src/binarysearch.java
+++ src/binarysearch.java
@@ -2,13 +2,26 @@ public class binarysearch {
     public static void main(String[] args) {
 
         int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int[] nums2 = {1,2,2,2,3,3,6,6,9,10};
+        int t = 3;
         int k = 6;
         int left = 0;
         int right = nums.length - 1;
+        int laenind = -1;
         int indk = -1;
 
+        while (left<=right){
+            int mid = left + (right-left) + 1;
 
-
+            if (nums2[mid] == t) {
+                laenind = mid;
+                right = mid + 1;
+            } else if (nums[mid] < t) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
 
 //        while (left <= right){
 //            int mid = left + (right - left)/2;
