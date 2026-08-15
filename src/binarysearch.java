5c0cc844faa08b9ccce7e4a11f38e2f303b7b0c7
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-15 11:04:31 +0530

    binary search subtop

--- src/binarysearch.java
+++ src/binarysearch.java
@@ -7,18 +7,21 @@ public class binarysearch {
         int right = nums.length - 1;
         int indk = -1;
 
-        while (left <= right){
-            int mid = left + (right - left)/2;
-            
-            if (nums[mid] == k){
-                indk = mid;
-                break;
-            } else if (nums[mid] < k) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
+
+
+
+//        while (left <= right){
+//            int mid = left + (right - left)/2;
+//
+//            if (nums[mid] == k){
+//                indk = mid;
+//                break;
+//            } else if (nums[mid] < k) {
+//                left = mid + 1;
+//            } else {
+//                right = mid - 1;
+//            }
+//        }
 
         System.out.println("The index of the Target is :" + indk);
 
