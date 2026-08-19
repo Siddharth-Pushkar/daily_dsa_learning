ef2b2465ddf7bb334bb693bd68e436993d3f912b
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-19 15:32:12 +0530

    linkedlist

--- src/linkedlist.java
+++ src/linkedlist.java
@@ -7,15 +7,13 @@ class ListNode {
         this.next = null;
     }
 }
-
 public class linkedlist {
-
     public static void main(String[] args) {
 
-        ListNode a = new ListNode(10);
-        ListNode b = new ListNode(20);
-        ListNode c = new ListNode(30);
-        ListNode d = new ListNode(40);
+        ListNode a = new ListNode(5);
+        ListNode b = new ListNode(8);
+        ListNode c = new ListNode(12);
+        ListNode d = new ListNode(17);
 
         ListNode head = a;
 
@@ -23,6 +21,11 @@ public class linkedlist {
         b.next = c;
         c.next = d;
         ListNode current = head;
+
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
 
 
 
