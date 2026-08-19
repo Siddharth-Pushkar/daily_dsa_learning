b1ad071e4c35c61b76b710d1bd74a8dde307d475
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-19 15:01:59 +0530

    linkedlist

--- src/linkedlist.java
+++ src/linkedlist.java
@@ -1,5 +1,24 @@
 public class linkedlist {
+
+
+
     public static void main(String[] args) {
+        class ListNode {
+            int val;
+            ListNode next;
+        }
+
+
+        ListNode a = new ListNode(10);
+        ListNode b = new ListNode(20);
+        ListNode c = new ListNode(30);
+        ListNode d = new ListNode(40);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+
+
 
     }
 }
