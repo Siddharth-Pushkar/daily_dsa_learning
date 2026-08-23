90bdf0482fb5c956ebe0650935b7c060312219c1
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-23 16:16:07 +0530

    linkedlist practice

--- src/linkedlistpractice.java
+++ src/linkedlistpractice.java
@@ -1,6 +1,17 @@
+
 public class linkedlistpractice {
 
     public static void main(String[] args) {
+
+        ListNode a = new ListNode(10);
+        ListNode b = new ListNode(20);
+        ListNode c = new ListNode(30);
+        ListNode d = new ListNode(40);
+        ListNode e = new ListNode(50);
+        ListNode head = a;
+
+        ListNode current = head;
+
 
     }
 }
