2c260ef0195b282e726001827f5c194f29f73fbd
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-23 16:23:30 +0530

    linkedlist practice

--- src/linkedlistpractice.java
+++ src/linkedlistpractice.java
@@ -11,22 +11,27 @@ public class linkedlistpractice {
         ListNode head = a;
 
         ListNode current = head;
+        ListNode current2 = head;
         a.next = b;
         b.next = c;
         c.next = d;
         d.next = e;
         e.next = null;
         int count = 0;
+        int target = 40;
+
+        while (current2 != null){
+
+            System.out.print(current2.val + " ");
+            current2 = current2.next;
+        }
 
         while (current != null){
-
-            System.out.print(current + " ");
+            System.out.print(current.val + " ");
             current = current.next;
             count++;
         }
-
         System.out.println();
-
         System.out.println("Number of nodes are: "+count);
     }
 }
