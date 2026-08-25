1e548144beef871a6bb2d6a8adfd198142507e15
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-08-25 19:26:04 +0530

    bubble sort built the algo myself

---
+++ src/bubblesorting.java
@@ -0,0 +1,18 @@
+public class bubblesorting {
+    public static void main(String[] args) {
+        int[] nums = {7,3,6,2,5};
+
+        for(int i = 1; i<nums.length; i++){
+            for (int j = 0; j<nums.length-i;j++){
+                if (nums[j] > nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+        for (int x : nums){
+            System.out.print(x+" ");
+        }
+    }
+}
