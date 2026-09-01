efe8aff44a7684e6bd4b236c932722ce50d2120d
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-09-01 14:51:40 +0530

    stack operations

--- src/stacks.java
+++ src/stacks.java
@@ -1,4 +1,15 @@
 public class stacks {
+    private int[] arr;
+    private int capacity;
+    private int top;
+
+    void push(int x) {
+        if (top == arr.length-1) {
+            System.out.println("Stack Overflow");
+            return; }
+
+        arr[top] = x; top++;
+    }
 
     public static void main(String[] args) {
 
