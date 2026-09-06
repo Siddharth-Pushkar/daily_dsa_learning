3c614553c214e328409cb8a5ce8729cdf21bf4ba
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-09-06 11:49:20 +0530

    stack operations

---
+++ src/stackslibraries.java
@@ -0,0 +1,22 @@
+
+import java.util.Stack;
+
+public class stackslibraries {
+    public static void main(String[] args) {
+        Stack<Integer> stger = new Stack<>();
+
+        stger.push(2);
+        stger.push(5);
+        stger.push(7);
+        stger.push(9);
+        stger.push(2);
+        stger.push(10);
+
+        System.out.println(stger.peek());
+        System.out.println(stger.pop());
+        System.out.println(stger.peek());
+
+
+
+    }
+}
