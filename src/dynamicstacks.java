defd06747d547afd7a612294962fa15845270249
Siddharth Pushkar <siddharthpushkar04@gmail.com>
2026-09-11 10:26:01 +0530

    stack operations

--- src/dynamicstacks.java
+++ src/dynamicstacks.java
@@ -1,23 +1,23 @@
 public class dynamicstacks extends stacks {
 
-
     public dynamicstacks(int size){
         super(size); // it will call stacks size from this
     }
-
     @Override
     public void push(int x) {
 
-        this 
+        // this one triggers when the stack is full and then the size is increased by double to compensate.
         if (isFull() == true) {
             int[] temp = new int[arr.length * 2];
 
             for (int i = 0; i< arr.length; i++){
                 temp[i] = arr[i];
-            }
 
+            }
             arr = temp;
+            top++; // it is pointing now to the next empty spot
         }
+        // then we will insert the next value into the stacks
         arr[top] = x;
         top++;
     }
